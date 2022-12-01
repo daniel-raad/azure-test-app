2edb7e61a0c8eaffa09699780e09182096ce78f9
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-12-01 11:23:12 +0000

    wip1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import NavbarExternal from './Components/NavbarExternal';
+import ForYou from './Pages/ClientApp/ForYou';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header"> */}
+      <NavbarExternal />
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/live-event">
+            <LiveEvent />
+          </Route> */}
+            <Route path="/">
+              <ForYou />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      {/* </header> */}
     </div>
   );
 }
