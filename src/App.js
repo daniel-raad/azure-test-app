1ee33fc2f8042a9fa9738c8dc6b2665735c1ceb5
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-12-01 12:50:52 +0000

    wip12

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header"> */}
       {/* </header> */}
-      <p>hello world</p>
+      <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: 'primary.dark',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    />
       <a href='https://mlgsapplication.azurewebsites.net/?groupId=842c1560-7174-11ed-8d50-85d682ce954a'>go to call</a>
     </div>
   );
